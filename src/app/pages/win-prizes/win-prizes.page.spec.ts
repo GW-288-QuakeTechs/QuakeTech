17450d564aa319b4919a9379ef0c2436ea0de7e9
Lahiru <lahiru7675@gmail.com>
2023-08-17 12:53:14 +1000

    startup code

---
+++ src/app/pages/win-prizes/win-prizes.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { WinPrizesPage } from './win-prizes.page';
+
+describe('WinPrizesPage', () => {
+  let component: WinPrizesPage;
+  let fixture: ComponentFixture<WinPrizesPage>;
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(WinPrizesPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/win-prizes/win-prizes.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-win-prizes',
+  templateUrl: './win-prizes.page.html',
+  styleUrls: ['./win-prizes.page.scss'],
+})
+export class WinPrizesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
