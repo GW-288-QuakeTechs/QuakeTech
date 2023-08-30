5b5b36547e68832dccbcbf643670d9bb2e6ec74b
S3896446 <s3896446@student.rmit.edu.au>
2023-08-30 12:16:44 +1000

    CSS Changes

--- src/app/pages/loader/loader.page.ts
+++ src/app/pages/loader/loader.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 
 @Component({
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoaderPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+    // Navigate to a different page after a 5-second delay
+    setTimeout(() => {
+      this.navigateToDifferentPage();
+    }, 5000); // 5000 milliseconds = 5 seconds
   }
 
+  private navigateToDifferentPage() {
+    this.navCtrl.navigateForward('/signup');
+  }
 }
